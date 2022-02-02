a64f9b4895a5503516155ddd9732d29274fb885f
kika <kikamozga18@ya.ru>
2022-02-02 16:31:07 +0300

    added TaskGroup and TaskGroupId in child

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,19 +1,23 @@
 using System;
+
 namespace YourTasks.Models
 {
     public class Task
     {
         public Guid Id { get; set; }
         public string? Text { get; set; }
+        public DateTime CreationDateTime { get; set; }
         public bool IsCompleted { get; set; }
         public string? Description { get; set; }
+        public Guid TaskGroupId { get; set; }
     }
 
     public class Subtask
     {
         public Guid Id { get; set; }
-        public Guid TaskId { get; set; }
         public string? Text { get; set; }
+        public DateTime CreationDateTime { get; set; }
         public bool IsCompleted { get; set; }
+        public Guid TaskId { get; set; }
     }
 }
---
+++ Models/TaskGroup.cs
@@ -0,0 +1,10 @@
+using System;
+namespace YourTasks.Models
+{
+    public class TaskGroup
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set;}
+        public string? ElipceColor { get; set; }
+    }
+}
