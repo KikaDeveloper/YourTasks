3bbe1fe65ff2626d7177281c1c89f1c16b7be474
kika <kikamozga18@ya.ru>
2022-02-05 20:31:59 +0300

    added property and binding

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ReactiveUI;
 
 namespace YourTasks.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        
+        private TasksViewModel? _tasksViewModel;
+        public TasksViewModel TasksViewModel
+        {
+            get => _tasksViewModel!;
+            set => this.RaiseAndSetIfChanged(ref _tasksViewModel, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            TasksViewModel = new TasksViewModel(){
+
+            };
+        }
     }
 }
