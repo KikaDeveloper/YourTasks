6547944ba028f1dc988967058fcd9cd58cbdb79b
kika <kikamozga18@ya.ru>
2022-02-05 20:41:11 +0300

    added new model

---
+++ Models/Project.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YourTasks.Models
+{
+    public class Project
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? EllipseColor { get; set; }
+    }
+}
