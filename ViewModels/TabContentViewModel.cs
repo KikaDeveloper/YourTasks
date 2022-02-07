ca52e5d3c9a7ea194e8241e1606b04d0fef1703e
kika <kikamozga18@ya.ru>
2022-02-07 10:31:17 +0300

    added properties

--- ViewModels/TabContentViewModel.cs
+++ ViewModels/TabContentViewModel.cs
@@ -1,10 +1,30 @@
 using ReactiveUI;
+using System.Collections.ObjectModel;
 using YourTasks.Models;
 
 namespace YourTasks.ViewModels
 {
-    public class TabContentViewModel
+    public class TabContentViewModel : ViewModelBase
     {
-        
+        private ObservableCollection<Task>? _totalTasks;
+        private ObservableCollection<Task>? _comletedTasks;
+
+        public ObservableCollection<Task> TotalTasks
+        {
+            get => _totalTasks!;
+            set => this.RaiseAndSetIfChanged(ref _totalTasks, value);
+        }
+
+        public ObservableCollection<Task> CompletedTasks
+        {
+            get => _comletedTasks!;
+            set => this.RaiseAndSetIfChanged(ref _comletedTasks, value);
+        }
+
+        public TabContentViewModel()
+        {
+
+        }
+
     }
 }
