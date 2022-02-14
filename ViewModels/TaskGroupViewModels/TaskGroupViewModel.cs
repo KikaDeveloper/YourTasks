43cd6dda8797cb9c22c1b9a10ccd628bc307c150
KikaDeveloper <kikamozga18@ya.ru>
2022-02-14 21:08:00 +0300

    class is made abstract

--- ViewModels/TaskGroupViewModels/TaskGroupViewModel.cs
+++ ViewModels/TaskGroupViewModels/TaskGroupViewModel.cs
@@ -1,30 +1,11 @@
-using ReactiveUI;
 using System.Collections.ObjectModel;
-using YourTasks.Models;
 
 namespace YourTasks.ViewModels
 {
-    public class TaskGroupViewModel : ViewModelBase
+    public abstract class TaskGroupViewModel : ViewModelBase
     {
-        private string? _groupName;
-        private ObservableCollection<TaskViewModel>? _tasks;
-
-        public string GroupName
-        {
-            get => _groupName!;
-            set => this.RaiseAndSetIfChanged(ref _groupName, value);
-        }
-
-        public ObservableCollection<TaskViewModel> Tasks
-        {
-            get => _tasks!;
-            set => this.RaiseAndSetIfChanged(ref _tasks, value);
-        }
-
-        public TaskGroupViewModel()
-        {
-
-        }
+        public abstract string GroupName { get; set; }
+        public abstract ObservableCollection<TaskViewModel> Tasks { get; set; }
     }
 
 }
