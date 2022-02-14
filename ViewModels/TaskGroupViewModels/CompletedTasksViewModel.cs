f3a27447f24c544702e818d2c883c0cd1b58bc00
KikaDeveloper <kikamozga18@ya.ru>
2022-02-14 21:07:14 +0300

    Create CompletedTasksViewModel.cs

---
+++ ViewModels/TaskGroupViewModels/CompletedTasksViewModel.cs
@@ -0,0 +1,22 @@
+using ReactiveUI;
+using System.Collections.ObjectModel;
+
+namespace YourTasks.ViewModels
+{
+    public class CompletedTasksViewModel : TaskGroupViewModel
+    {
+        private string? _groupName;
+        private ObservableCollection<TaskViewModel>? _tasks;
+
+        public override string GroupName { 
+            get => _groupName!; 
+            set => this.RaiseAndSetIfChanged(ref _groupName, value);
+        }
+
+        public override ObservableCollection<TaskViewModel> Tasks { 
+            get => _tasks!; 
+            set => this.RaiseAndSetIfChanged(ref _tasks, value); 
+        }
+
+    }
+}
