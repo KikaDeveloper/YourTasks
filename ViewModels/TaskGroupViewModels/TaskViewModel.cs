3100c9fd85027c18309da18a88d8e4b1970f2c74
KikaDeveloper <kikamozga18@ya.ru>
2022-02-14 16:06:03 +0300

    Create TaskViewModel.cs

---
+++ ViewModels/TaskGroupViewModels/TaskViewModel.cs
@@ -0,0 +1,30 @@
+using ReactiveUI;
+using YourTasks.Models;
+using System.Collections.ObjectModel;
+
+namespace YourTasks.ViewModels
+{
+    public class TaskViewModel : ViewModelBase
+    {
+        private Task? _task;
+        private ObservableCollection<Task>? _subTasks;
+
+        public Task Task
+        {
+            get => _task!;
+            set => this.RaiseAndSetIfChanged(ref _task, value);
+        }
+
+        public ObservableCollection<Task> SubTasks
+        {
+            get => _subTasks!;
+            set => this.RaiseAndSetIfChanged(ref _subTasks, value);
+        }
+
+        public TaskViewModel()
+        {
+            
+        }
+
+    }
+}
