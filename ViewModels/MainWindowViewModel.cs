cb1bc81e3d45a12f0ea1e80ff9de65b1d063208e
Kirill Vasiliev <kikamozga18@ya.ru>
2022-02-16 16:47:47 +0300

    added projects property

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,20 @@
 ﻿using ReactiveUI;
+using System.Collections.ObjectModel;
 
 namespace YourTasks.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private TasksViewModel? _tasksViewModel;
-        public TasksViewModel TasksViewModel
-        {
-            get => _tasksViewModel!;
-            set => this.RaiseAndSetIfChanged(ref _tasksViewModel, value);
-        }
+        private ObservableCollection<ProjectViewModel>? _projects;
 
+        public ObservableCollection<ProjectViewModel> Projects
+        {
+            get => _projects!;
+            set => this.RaiseAndSetIfChanged(ref _projects, value);
+        }
+        
         public MainWindowViewModel()
         {
-            TasksViewModel = new TasksViewModel(){
-
-            };
         }
     }
 }
