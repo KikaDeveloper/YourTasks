c47541550522606b044fe1e78e8d558663acc691
Kirill Vasiliev <kikamozga18@ya.ru>
2022-02-16 15:43:59 +0300

    Update Task.cs

--- Models/Task.cs
+++ Models/Task.cs
@@ -9,15 +9,6 @@ namespace YourTasks.Models
         public DateTime CreationDateTime { get; set; }
         public bool IsCompleted { get; set; }
         public string? Description { get; set; }
-        public Guid TaskGroupId { get; set; }
-    }
-
-    public class Subtask
-    {
-        public Guid Id { get; set; }
-        public string? Text { get; set; }
-        public DateTime CreationDateTime { get; set; }
-        public bool IsCompleted { get; set; }
-        public Guid TaskId { get; set; }
+        public Guid ProjectId { get; set; }
     }
 }
