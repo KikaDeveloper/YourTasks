9c61ceec97931bd0723ddaa71d3cb3dc9f33f897
Kirill Vasiliev <kikamozga18@ya.ru>
2022-02-18 20:11:15 +0300

    Create NewTaskWindowViewModel.cs

---
+++ ViewModels/NewTaskWindowViewModel.cs
@@ -0,0 +1,12 @@
+using ReactiveUI;
+
+namespace YourTasks.ViewModels
+{
+    public class NewTaskWindowViewModel : ViewModelBase
+    {
+        public NewTaskWindowViewModel()
+        {
+
+        }
+    }
+}
