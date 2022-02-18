16f7e097dcf070b6ead7eeec8b0df8cf86788516
Kirill Vasiliev <kikamozga18@ya.ru>
2022-02-18 12:58:01 +0300

    warning fix

--- Models/TaskBase.cs
+++ Models/TaskBase.cs
@@ -6,7 +6,7 @@ namespace YourTasks.Models
     {
         public abstract int Id { get; set; }
         public abstract string? Text { get; set; }
-        public abstract string CreationDateTime { get; set; }
+        public abstract string? CreationDateTime { get; set; }
         public abstract bool IsCompleted { get; set; }
         public abstract string? Description { get; set; }
     }
