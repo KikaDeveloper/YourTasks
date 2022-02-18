70f723a6418db53aa47e3aba15075e52da027f68
Kirill Vasiliev <kikamozga18@ya.ru>
2022-02-18 14:41:28 +0300

    add load from db method

--- Services/AppRepository.cs
+++ Services/AppRepository.cs
@@ -1,5 +1,7 @@
 using SQLite;
+using System.Linq;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using YourTasks.Models;
 
 namespace YourTasks.Services
@@ -20,7 +22,7 @@ namespace YourTasks.Services
         protected AppRepository()
         {
             connection = new SQLiteAsyncConnection("tasks.db");
-            EnsureCreateTables().GetAwaiter();
+            System.Threading.Tasks.Task.Run(async() => await EnsureCreateTables());
         }
 
         private async System.Threading.Tasks.Task EnsureCreateTables()
@@ -51,9 +53,27 @@ namespace YourTasks.Services
             await connection.UpdateAsync(entity);
         }
 
-        public async System.Threading.Tasks.Task<IEnumerable<TEntity>> GetAllEntites<TEntity>() where TEntity : new()
+        public async System.Threading.Tasks.Task<List<TEntity>> GetAllEntites<TEntity>() where TEntity : new()
         {
             return await connection.Table<TEntity>().ToListAsync();
+        }
+
+        public async System.Threading.Tasks.Task<List<Project>> GetAllProjects()
+        {
+            var subTasks = await GetAllEntites<SubTask>();
+            var tasks = await GetAllEntites<Task>();
+            var projects = await GetAllEntites<Project>();
+            
+            foreach(var project in projects)
+            {
+                var projectTasks = tasks.Where(task => task.Id == project.Id);
+                foreach(var task in projectTasks)
+                {
+                    task.SubTasks = new ObservableCollection<SubTask>(subTasks.Where(subTask => subTask.TaskId == task.Id));
+                }
+                project.Tasks = new ObservableCollection<Task>(projectTasks);
+            }
+            return projects;
         }
 
     }
