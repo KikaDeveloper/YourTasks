c626556b6dd89b0dd8f5a19b42c29b5e3e12aa0a
Kirill Vasiliev <kikamozga18@ya.ru>
2022-02-19 09:52:11 +0300

    add properties

--- ViewModels/NewTaskWindowViewModel.cs
+++ ViewModels/NewTaskWindowViewModel.cs
@@ -4,9 +4,27 @@ namespace YourTasks.ViewModels
 {
     public class NewTaskWindowViewModel : ViewModelBase
     {
+
+        private string? _text;
+        private string? _description;
+
+        public string Text
+        {
+            get => _text!;
+            set => this.RaiseAndSetIfChanged(ref _text, value);
+        }
+
+        public string Description
+        {
+            get => _description!;
+            set => this.RaiseAndSetIfChanged(ref _description, value);
+        }        
+
+        public IReactiveCommand AddNewTaskCommand { get; }
+
         public NewTaskWindowViewModel()
         {
-
+            AddNewTaskCommand = ReactiveCommand.Create(()=>{});
         }
     }
 }
