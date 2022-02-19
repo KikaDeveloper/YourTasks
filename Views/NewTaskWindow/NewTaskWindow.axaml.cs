2dde73f32f8332816b688e8b6f432d78808b34b1
Kirill Vasiliev <kikamozga18@ya.ru>
2022-02-19 11:35:16 +0300

    add subsscribe for close

--- Views/NewTaskWindow/NewTaskWindow.axaml.cs
+++ Views/NewTaskWindow/NewTaskWindow.axaml.cs
@@ -1,10 +1,16 @@
+using System;
 using Avalonia;
+using Avalonia.Input;
 using Avalonia.Controls;
+using Avalonia.ReactiveUI;
 using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using YourTasks.ViewModels;
+using ReactiveUI;
 
 namespace YourTasks.Views
 {
-    public partial class NewTaskWindow : Window
+    public partial class NewTaskWindow : ReactiveWindow<NewTaskWindowViewModel>
     {
         public NewTaskWindow()
         {
@@ -12,6 +18,8 @@ namespace YourTasks.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
+
+            this.WhenActivated(d => ViewModel!.AddNewTaskCommand.Subscribe(Close));
         }
 
         private void InitializeComponent()
