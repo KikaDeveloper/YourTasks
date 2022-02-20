4e0d3379438b960532a5a25581bcea80f8442b53
Kirill Vasiliev <kikamozga18@ya.ru>
2022-02-20 09:57:43 +0300

    Update SubTaskViewModel.cs

--- ViewModels/SubTaskViewModel.cs
+++ ViewModels/SubTaskViewModel.cs
@@ -6,48 +6,19 @@ namespace YourTasks.ViewModels
 {  
     public class SubTaskViewModel : ViewModelBase
     {
-        private string? _text;
-        private string? _description;
-        private string? _creationDateTime;
-        private bool _isCompleted;
-
-        public string Text
+        private SubTask? _subTask;
+        public SubTask SubTask
         {
-            get => _text!;
-            set => this.RaiseAndSetIfChanged(ref _text, value);
-        }
-
-        public string Description
-        {
-            get => _description!;
-            set => this.RaiseAndSetIfChanged(ref _description, value);
-        }
-    
-        public string CreationDateTime
-        {
-            get => _creationDateTime!;
-            set => this.RaiseAndSetIfChanged(ref _creationDateTime, value);
-        }
-
-        public bool IsCompleted
-        {
-            get => _isCompleted;
-            set {
-                this.RaiseAndSetIfChanged(ref _isCompleted, value);
-                SubTaskCompletedEvent?.Invoke(this, new TaskCompletedArgs(value));
-            }
+            get => _subTask!;
+            set => this.RaiseAndSetIfChanged(ref _subTask, value);
         }
     
         public EventHandler? SubTaskDeleteEvent;
-        public EventHandler<TaskCompletedArgs>? SubTaskCompletedEvent;
         public IReactiveCommand DeleteTaskCommand { get; }
 
         public SubTaskViewModel(SubTask task)
         {
-            Text = task.Text!;
-            Description = task.Description!;
-            CreationDateTime = task.CreationDateTime!;
-            IsCompleted = task.IsCompleted!;
+            SubTask = task;
 
             DeleteTaskCommand = ReactiveCommand.Create(()=>{
                 SubTaskDeleteEvent?.Invoke(this, new EventArgs());
