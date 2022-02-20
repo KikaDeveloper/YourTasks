f3bd364996c7bce9c68b17fa638815eeffa251c4
Kirill Vasiliev <kikamozga18@ya.ru>
2022-02-20 09:44:01 +0300

    implement property changed for properties

--- Models/SubTask.cs
+++ Models/SubTask.cs
@@ -1,4 +1,5 @@
 using SQLite;
+using ReactiveUI;
 
 namespace YourTasks.Models
 {
@@ -7,16 +8,50 @@ namespace YourTasks.Models
     {
         [PrimaryKey, AutoIncrement]
         [Column("id")]
-        public override int Id { get; set; }
+        public override int Id 
+        { 
+            get => _id;
+            set => this.RaiseAndSetIfChanged(ref _id, value);
+        }
+
         [Column("text")]
-        public override string? Text { get; set; }
+        public override string? Text 
+        { 
+            get => _text!; 
+            set => this.RaiseAndSetIfChanged(ref _text, value); 
+        }
+
         [Column("creation_datetime")]
-        public override string? CreationDateTime { get; set; }
+        public override string? CreationDateTime 
+        { 
+            get => _creationDateTime!; 
+            set => this.RaiseAndSetIfChanged(ref _creationDateTime, value); 
+        }
+
         [Column("is_completed")]
-        public override bool IsCompleted { get; set; }
+        public override bool IsCompleted 
+        { 
+            get => _isCompleted; 
+            set {
+                this.RaiseAndSetIfChanged(ref _isCompleted, value);
+                TaskCompletedEvent?.Invoke(this, new TaskCompletedArgs(value));
+            }
+        }
+
         [Column("description")]
-        public override string? Description { get; set; }
+        public override string? Description 
+        { 
+            get => _description!; 
+            set => this.RaiseAndSetIfChanged(ref _description, value); 
+        }
+
         [Column("task_id")]
-        public int TaskId { get; set; }
+        public int TaskId 
+        { 
+            get => _taskId; 
+            set => this.RaiseAndSetIfChanged(ref _taskId, value);
+        }
+
+        protected int _taskId;
     }
 }
