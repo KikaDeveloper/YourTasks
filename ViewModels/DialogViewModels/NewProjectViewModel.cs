98391609eb38bf2bc6bc060eccd9e16236bd17e5
Kirill Vasiliev <kikamozga18@ya.ru>
2022-02-20 14:44:46 +0300

    Create NewProjectViewModel.cs

---
+++ ViewModels/DialogViewModels/NewProjectViewModel.cs
@@ -0,0 +1,58 @@
+using ReactiveUI;
+using ReactiveUI.Validation.Contexts;
+using ReactiveUI.Validation.Abstractions;
+using System;
+using System.Reactive;
+using YourTasks.Models;
+
+namespace YourTasks.ViewModels
+{
+    public class NewProjectViewModel : ViewModelBase, IValidatableViewModel
+    {
+        private string? _name;  
+
+        private string? _ellipseColor;
+        private string? _description; 
+
+        public string Name
+        {
+            get => _name!;
+            set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+
+        public string EllipseColor
+        {
+            get => _ellipseColor!;
+            set => this.RaiseAndSetIfChanged(ref _ellipseColor, value);
+        }
+
+        public string Description
+        {
+            get => _description!;
+            set => this.RaiseAndSetIfChanged(ref _description, value);
+        }
+
+        public ValidationContext ValidationContext { get; } = new ValidationContext();
+
+        public ReactiveCommand<Unit, Project> AddNewProjectCommand { get; }
+
+        public NewProjectViewModel()
+        {
+            var nameIsValid = this.WhenAnyValue(
+                x => x.Name,
+                (name) => !string.IsNullOrEmpty(name)
+            );
+
+            var colorIsValid = this.WhenAnyValue(
+                x => x.EllipseColor,
+                (color) => !string.IsNullOrEmpty(color)
+            );
+
+            AddNewProjectCommand = ReactiveCommand.Create(() 
+                => new Project());
+        }
+
+
+
+    }
+}
